b097ce60fab4c2a60cf80e1a2dab278b8422f999
Shubham Sharma <shubhamku044@gmail.com>
2023-09-14 00:43:29 +0530

    added tailwindcss

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,10 @@
-import './App.css'
-
 function App() {
 
   return (
     <>
-      <h1>Certi Ledger</h1>
+      <h1 className='text-red-500'>Certi Ledger</h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
