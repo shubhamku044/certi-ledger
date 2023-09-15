7a2c4c33415056a42fbab29a0f10812527b665fe
Shubham Sharma <shubhamku044@gmail.com>
2023-09-15 23:02:30 +0530

    added folder

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,10 @@
+import Registeration from './components/Registeration';
 function App() {
 
   return (
     <>
       <h1 className='text-red-500'>Certi Ledger</h1>
+      <Registeration />
     </>
   );
 }
