5f245543ffbdd8c5ccff1e76f67cc4380f8327ca
Ritam Nambiar <ritamnambiar@gmail.com>
2023-09-16 00:18:14 +0530

    register-user-api

--- server/src/app.ts
+++ server/src/app.ts
@@ -11,11 +11,45 @@ app.use(express.urlencoded({ extended: true, limit: '10kb' }));
 app.use(cors());
 
 app.get('/health', (_: Request, res: Response) => {
+  console.log('heyy')
   res.status(200).json({
     health: 'Up and running'
   });
 });
+app.post('/registerrecipient', (req: Request, res: Response) => {
+  try {
+    // Extract user data from the request body
+    const {
+      username,
+      password,
+      firstName,
+      lastName,
+      email,
+    } = req.body;
 
+    // You can perform any validation or database operations here if needed.
+
+    // Return the same data as a response
+    const userData = {
+      username,
+      password,
+      firstName,
+      lastName,
+      email,
+    };
+
+    res.status(200).json(userData);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'An error occurred while registering the recipient.' });
+  }
+});
+
+app.get('/', (_: Request, res: Response) => {
+  res.status(200).json({
+    health: 'Up and running'
+  });
+});
 app.use('*', (req: Request, res: Response) => {
   res.status(404).json({
     msg: `Can't find the ${req.originalUrl} in this server`
