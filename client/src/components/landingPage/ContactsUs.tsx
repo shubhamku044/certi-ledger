c11d69497ec8c629b35c1b95013ae62ba271f606
Shubham Sharma <shubhamku044@gmail.com>
2023-09-23 11:56:07 +0530

    added contact us section

--- client/src/components/landingPage/ContactsUs.tsx
+++ client/src/components/landingPage/ContactsUs.tsx
@@ -1,6 +1,10 @@
 const ContactsUs = () => {
   return (
-    <div>ContactsUs</div>
+    <footer className="">
+      <p className="mx-auto text-center py-6 font-semibold text-[#A755F1]/70">
+        ©2023 CertiLedger. All rights reserved.
+      </p>
+    </footer>
   );
 };
 
--- client/src/components/landingPage/Register.tsx
+++ client/src/components/landingPage/Register.tsx
@@ -1,15 +1,15 @@
 const Register = () => {
   return (
     <section
-      className="py-24 min-h-screen"
+      className="py-24 min-h-screen flex items-center justify-center"
       style={{
         background: 'linear-gradient(180deg, #170220, #2D0E43, #170220)'
       }}
     >
       <div className="max-w-7xl mx-auto flex items-center justify-center">
-        <div className="gap-6">
+        <div className="gap-20 flex flex-col items-center">
           <p
-            className="w-full text-center px-10 py-10 font-bold text-3xl tracking-wider italic"
+            className="text-center px-10 py-10 font-bold text-3xl tracking-wider italic"
             style={{
               background: 'linear-gradient(180deg, rgba(167, 85, 244, 0.50) 0%, rgba(123, 60, 180, 0.50) 10.43%, rgba(21, 2, 29, 0.50) 50.53%, rgba(192, 133, 246, 0.50) 99.99%, rgba(255, 255, 255, 0.00) 100%)',
               borderRadius: '2rem'
@@ -19,7 +19,7 @@ const Register = () => {
             Register now for secure, trusted certificate management. <br />
             Unlock the future of validation... <br />
           </p>
-          <button className="">Register Now</button>
+          <button className="border-4 px-10 py-2 rounded-lg bg-white text-[#6D30A4] font-black tracking-wide text-lg border-[#6d30a4] mx-auto">Register Now</button>
         </div>
       </div>
     </section>
